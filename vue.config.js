791e3ccc2d799c9a3a2dcbd419797e6c78220232
shypang <smartdragon417@gmail.com>
2022-10-11 11:20:52 +0900

    fix production in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "productgion" ? "/vue-corona-app" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-corona-app" : "/",
   outputDir: "docs",
   transpileDependencies: true,
 });
